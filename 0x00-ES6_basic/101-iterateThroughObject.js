7f0cdaa822bac4f59df6818234a30cf2aa0cede8
merytpeters <merytpeters@gmail.com>
2024-10-01 02:00:19 +0100

    Iterate through objects

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employeesList = [];
+
+  for (const employee of reportWithIterator) {
+    employeesList.push(employee);
+  }
+  return employeesList.join(' | ');
+}
