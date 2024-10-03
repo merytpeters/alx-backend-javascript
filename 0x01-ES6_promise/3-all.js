4993e042272d5bf271878fbcc8e6ee093c6db19e
merytpeters <merytpeters@gmail.com>
2024-10-03 21:20:02 +0100

    Multuple succesful promises , trying to meet requirements

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch((error) => {
-      console.log('Signup System offline', error);
+    .catch(() => {
+      console.log('Signup System offline');
     });
 }
