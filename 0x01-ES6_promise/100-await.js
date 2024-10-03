c9217c87301bc3efb3d1586f35764ac0a3aa0fdb
merytpeters <merytpeters@gmail.com>
2024-10-03 22:35:22 +0100

    Await/Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+
+  return {
+    photo,
+    user,
+  };
+}
