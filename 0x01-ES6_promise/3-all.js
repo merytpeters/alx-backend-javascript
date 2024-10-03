2f2c214d42c6c0b03b9be7d9930d6db954aa2aa7
merytpeters <merytpeters@gmail.com>
2024-10-03 19:46:36 +0100

    Multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-    return Promise.all(uploadPhoto, createUser)
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
+    })
+    .catch((error) => {
+      console.log('Signup System offline', error);
+    });
 }
