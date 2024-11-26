55037d1916d3e1c08b1d67ea59ae34488654b9ae
merytpeters <merytpeters@gmail.com>
2024-11-26 11:29:42 +0100

    Stdin, pause used to end the first stdin process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (INPUT) => {
   process.stdout.write(`Your name is: ${INPUT.toString().trim()}\n`);
+  process.stdin.pause();
 });
 
 process.stdin.on('end', () => {
