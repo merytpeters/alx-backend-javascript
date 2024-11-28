7a00c2ac97faa973086ea7afe3be205be90a0c00
merytpeters <merytpeters@gmail.com>
2024-11-28 19:11:41 +0100

    Test modified to check the numbers not just the sum

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,23 +3,19 @@ const calculateNumber = require('./0-calcul');
 
 
 describe('calculateNumber', function() {
-  it('should return the correct sum of positive numbers', function () {
-    const result = calculateNumber(1, 3);
-    assert.strictEqual(result, 4);
+  it('should return the sum of two rounded numbers', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('should return the correct rounded sum of positive numbers', function(){
-    const result = calculateNumber(1, 3.7);
-    assert.strictEqual(result, 5);
+  it('should return the sum of two rounded numbers', function(){
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return the correct rounded sum of positive numbers', function(){
-    const result = calculateNumber(1.2, 3.7);
-    assert.strictEqual(result, 5);
+  it('should return the sum of two rounded numbers', function(){
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return the correct rounded sum of positive numbers', function(){
-    const result = calculateNumber(1.5, 3.7);
-    assert.strictEqual(result, 6);
+  it('should return the sum of two rounded numbers', function(){
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
